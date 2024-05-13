f5fd638291ae2755ed220ca1e5930d7e4a39da5a
cdreyer <cristianbr.dreyer@gmail.com>
2024-05-13 00:56:30 -0300

    fix filters, optmize search

--- src/app/api/pet/route.ts
+++ src/app/api/pet/route.ts
@@ -5,36 +5,9 @@ import Pet from "@/models/pet";
 
 export async function GET(req: NextRequest): Promise<Response> {
    try {
-      await connectToDatabase();
-
-      const params = req.nextUrl.searchParams;
-
-      const petFilter = {} as StrictFilter<Pet>;
-
-      if (params.has('type')) {
-         petFilter.type = params.get('type') as string;
-      }
-
-      if (params.has('size')) {
-         // get all values from the query string
-         petFilter.size = { $all: params.getAll('size') };
-      }
-
-      if (params.has('age')) {
-         petFilter.age = { $all: params.getAll('age') };
-      }
-
-      if (params.has('breed')) {
-         petFilter.breeds = { $all: params.getAll('breed') };
-      }
-
-      var result =
-         await collections
-            .pets?.find(petFilter)
-            .collation({ strength: 2 , locale: 'pt'})
-            .limit(30)
-            .toArray();
-
+      const filter = getPetFilterFromParams(req.nextUrl.searchParams);
+      const result = await searchPets(filter);
+      console.log(`amount of pets: ${result.length}`);
       return new Response(JSON.stringify(result));
    }
    catch (error) {
@@ -56,4 +29,51 @@ export async function POST(req: NextRequest): Promise<Response> {
    await collections.pets?.insertOne(pet);
 
    return new Response('ok');
+}
+
+
+async function searchPets(filter: Filter<Pet>): Promise<Pet[]> {
+   if (!collections.pets)
+      await connectToDatabase();
+
+   const result = await collections.pets!.find(filter).collation({ strength: 2, locale: 'pt' }).limit(30).toArray();
+   return result;
+}
+
+function getPetFilter(pet: Pet): Filter<Pet> {
+   const cleanFilter = Object.entries(pet).reduce((acc: Filter<Pet>, [key, value]) => {
+      if (typeof value === 'string') {
+         acc[key] = { $regex: new RegExp(value, 'i') }; // Busca de texto aproximada (case-insensitive)
+      } else {
+         acc[key] = { $in: value }; // Multi seleção
+      }
+      return acc;
+   }, {} as Pet);
+
+   // Remover campos vazios do filtro
+   Object.keys(cleanFilter).forEach((key) => (cleanFilter[key as keyof Pet] == undefined) && delete cleanFilter[key as keyof Pet]);
+
+   return cleanFilter;
+}
+
+function getPetFilterFromParams(params: URLSearchParams): Filter<Pet> {
+   const pet = {} as Pet;
+   if (params.has('type')) {
+      pet.type = params.get('type') as string;
+   }
+
+   if (params.has('size')) {
+      pet.size = params.getAll('size');
+   }
+
+   if (params.has('age')) {
+      pet.age = params.getAll('age');
+   }
+
+   if (params.has('breed')) {
+      pet.breeds = params.getAll('breed');
+   }
+
+   const petFilter = getPetFilter(pet);
+   return petFilter;
 }
