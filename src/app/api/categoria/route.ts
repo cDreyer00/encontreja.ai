584f71024ff5203afa872316af162793a36fcbfd
cdreyer <cristianbr.dreyer@gmail.com>
2024-05-21 15:55:46 -0300

    locations category

--- src/app/api/categoria/route.ts
+++ src/app/api/categoria/route.ts
@@ -1,16 +1,22 @@
 import { NextRequest } from "next/server";
+import { connectToDatabase, collections } from "@/services/db";
 
 export async function GET(req: NextRequest): Promise<Response> {
    const params = req.nextUrl.searchParams;
    const category = params.get('q');
+   
+   if(category === 'abrigo'){
+      let locations = await getAllLocations();
+      return new Response(JSON.stringify(locations));      
+   }
 
    const res =
       category === 'raca_cachorro' ? dogBreeds :
-      category === 'raca_gato'     ? catBreeds :
-      category === 'cor'           ? colors :
-      category === 'idade'         ? ages :
-      category === 'tamanho'       ? sizes :
-      category === 'genero'        ? genders :
+      category === 'raca_gato' ? catBreeds :
+      category === 'cor' ? colors :
+      category === 'idade' ? ages :
+      category === 'tamanho' ? sizes :
+      category === 'genero' ? genders :
       {
          dogBreeds,
          catBreeds,
@@ -19,8 +25,22 @@ export async function GET(req: NextRequest): Promise<Response> {
          sizes,
          genders
       }
-
+      
    return new Response(JSON.stringify(res));
+}
+
+async function getAllLocations(): Promise<string[]> {
+   if (!collections.pets)
+      await connectToDatabase();
+
+   let col = collections.pets;
+   if (!col)
+      throw new Error('Error connecting to database');
+
+   // get all values from location field
+   let locations = await col.distinct('location');
+   locations = locations.filter((location) => location && location.trim().length > 0);
+   return locations as string[];
 }
 
 const dogBreeds = [
@@ -85,7 +105,7 @@ const ages = [
    'Idoso'
 ]
 
-const sizes =[
+const sizes = [
    'Incerto',
    'Mini',
    'Pequeno',
