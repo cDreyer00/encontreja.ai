f2f849346d68eb1a16c6f26d9d0594595fa4269f
cdreyer <cristianbr.dreyer@gmail.com>
2024-05-30 15:29:50 -0300

    debug img req

--- src/app/api/image/route.ts
+++ src/app/api/image/route.ts
@@ -2,26 +2,25 @@ import { submitImageToDrive, getImageUrlFromDrive, submitImgUrlToDrive } from "@
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-   // let body = await exportBody(req);
    console.log("=================================== START ===================================")
-   let body = await req.json();
+   let body = await exportBody(req);
+
    console.log(body)
    console.log('=================================== FINISH ===================================')
-   return new Response('OK', { status: 200 });
 
-   // if (body.isUrl) {
-   //    let fileId = await submitImgUrlToDrive(body.folderId, body.img as string);
-   //    if (!fileId) return new Response('Failed to submit image URL', { status: 500 });
-   //    let url = getImageUrlFromDrive(fileId);
-   //    let data = { fileId: fileId, imgUrl: url }
-   //    return new Response(JSON.stringify(data), { status: 200 });
-   // }
+   if (body.isUrl) {
+      let fileId = await submitImgUrlToDrive(body.folderId, body.img as string);
+      if (!fileId) return new Response('Failed to submit image URL', { status: 500 });
+      let url = getImageUrlFromDrive(fileId);
+      let data = { fileId: fileId, imgUrl: url }
+      return new Response(JSON.stringify(data), { status: 200 });
+   }
 
-   // let fileId = await submitImageToDrive(body.folderId, body.img as File);
-   // if (!fileId) return new Response('Failed to submit image', { status: 500 });
-   // let url = getImageUrlFromDrive(fileId);
-   // let data = { fileId: fileId, imgUrl: url }
-   // return new Response(JSON.stringify(data), { status: 200 });
+   let fileId = await submitImageToDrive(body.folderId, body.img as File);
+   if (!fileId) return new Response('Failed to submit image', { status: 500 });
+   let url = getImageUrlFromDrive(fileId);
+   let data = { fileId: fileId, imgUrl: url }
+   return new Response(JSON.stringify(data), { status: 200 });
 }
 
 interface ImgReq {
@@ -31,22 +30,20 @@ interface ImgReq {
 }
 
 async function exportBody(req: NextRequest): Promise<ImgReq> {
-   try {
+   let dataType = req.headers.get('content-type');
+   let isJson = dataType?.includes('application/json');
+
+   if (!isJson) {
       let data = await req.formData();
       let img = data.get('image') as File;
       let folderId = data.get('folderId') as string;
       return { img, folderId, isUrl: false };
-   } catch (e) { }
-
-   try {      
+   }
+   else {
       let data = await req.json();
       data.img = data.imgUrl as string;
       data.isUrl = true;
       console.log("JSON data:", data);
       return data;
-   } catch (e) {
-      console.error('Failed to parse request body as JSON');
    }
-
-   throw new Error('Failed to parse request body');
 }
